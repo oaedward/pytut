f81a044595e9ff815c80b1b2a6b658701de5fe24
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-12-22 19:07:59 +0000

    initial commit

---
+++ employee/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Employee
+# Create your views here.
+
+def index(request):
+  myEmployees = Employee.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'myEmployees': myEmployees
+  }
+  return HttpResponse(template.render(context, request))
